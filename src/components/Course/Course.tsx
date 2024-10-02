34edf2812a50c56a6f4c42d0309c768eb307a5bc
didusanatoliy <bereg.007@mail.ru>
2024-10-03 01:10:16 +0300

    надо доработать

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -1,3 +1,49 @@
-export default function Course() {
-	return <div></div>
+export type courseType = {
+	id: number,
+	name: string,
+	src: string,
+	duration: string,
+	timeaday: string,
+	level: string,
+}
+export type CourseOneType = {
+	course: courseType,
+}
+export default function Course({ course }: CourseOneType) {
+	const { name, duration, timeaday, level, src } = course
+	return (
+		<>
+			<div className="relative shadow-lg max-w-[360px] mt-8 mb-2 flex flex-col self-start rounded-3xl bg-white sm:shrink-0 sm:grow sm:basis-0 box-border">
+				<div className="  mx-[0] mt-[0] max-w-[360px] max-h-[501px]">
+					<img className="rounded-3xl" src={src}></img>
+					<svg className="absolute top-[20px] right-[40px]">
+						<use xlinkHref="/src/assets/img/icon/sprite.svg#icon-plus" />
+					</svg>
+				</div>
+				<div className="p-[6] mb-[4] text-base grid md:gap-[3] ">
+					<h6 className="font-bold text-[32px] my-6">{name}</h6>
+					<div className="flex justify-start">
+						<div className=" flex justify-start rounded-3xl bg-[#F7F7F7] px-[3] py-[3] mr-[2]">
+							<svg className="w-[18px] h-[18px]">
+								<use xlinkHref="/src/assets/img/icon/sprite.svg#calendar" />
+							</svg>
+							<div className="pl-[2]" font-bold >{duration}</div>
+						</div>
+						<div className="flex justify-start rounded-3xl shadow-lg bg-[#F7F7F7] px-[3] py-[3] ">
+							<svg className="w-[18px] h-[18px]">
+								<use xlinkHref="/src/assets/img/icon/sprite.svg#time" />
+							</svg>
+							<div className="pl-[2]">{timeaday}</div>
+						</div>
+					</div>
+					<div className="flex justify-start rounded-3xl bg-[#F7F7F7] px-[3] py-[3] mr-[24] ">
+						<svg className="w-[18px] h-[18px]">
+							<use xlinkHref="/src/assets/img/icon/sprite.svg#difficulty_signal" />
+						</svg>
+						<div className="pl-[2]">{level}</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,84 @@
+import Course from '../Course/Course'
 import Header from '../Header/Header'
+import yoga from '/src/assets/img/yoga2.png'
+import stretching from '/src/assets/img/stretching2.png'
+import zumba from '/src/assets/img/zumba2.png'
+import bodyflex from '/src/assets/img/bodyflex2.png'
+import step from '/src/assets/img/step.png'
+import Button from '../Button/Button'
 
 export default function Main() {
+	const courses = [
+		{
+			id: 1,
+			name: "Йога",
+			src: yoga,
+			duration: "25дней",
+			timeaday: "20-25минут/день",
+			level: "Сложность",
+		},
+		{
+			id: 2,
+			name: "Стретчинг",
+			src: stretching,
+			duration: "25дней",
+			timeaday: "20-25минут/день",
+			level: "Сложность",
+		},
+		{
+			id: 3,
+			name: "Зумба",
+			src: zumba,
+			duration: "25дней",
+			timeaday: "20-25минут/день",
+			level: "Сложность",
+		},
+		{
+			id: 4,
+			name: "Степ-аэробика",
+			src: step,
+			duration: "25дней",
+			timeaday: "20-25минут/день",
+			level: "Сложность",
+		},
+		{
+			id: 5,
+			name: "Бодифлекс",
+			src: bodyflex,
+			duration: "25дней",
+			timeaday: "20-25минут/день",
+			level: "Сложность",
+		}
+	]
 	return (
 		<main>
 			<Header />
+			<div className="bg-gray-[100] grid place-content-center">
+				<div className=" max-w-[1440px]">
+					<div className="flex justify-between my-[50px] relative">
+						<div className="text-[60px] h-[120px] text-pretty inline-block align-middle"> Начните заниматься спортом и улучшите качество жизни</div>
+						<div className="mt-[6] p-[4] text-[32px] min-w-[288px] max-h-[120px] rounded-md bg-[#BCEC30]">Измени своё тело за полгода!</div>
+						<div className="absolute top-[134px] right-[150px]">
+							<img src="/src/assets/img/polygon.png" alt="polygon" />
+						</div>
+
+					</div>
+					<div className="grid-cols-1 sm:grid md:grid-cols-3 -mr-[10] gap-[10]">
+						{courses.map(course =>
+							<Course course={course} key={course.id} />
+						)}
+					</div>
+					<div className="flex justify-center ">
+						<Button
+							width='w-[103px]'
+							background='bg-green_bg'
+							hover='bg-hover'
+							active='bg-active active:text-white'
+							title='Наверх ↑'
+						/>
+					</div>
+				</div>
+			</div>
 		</main>
 	)
 }

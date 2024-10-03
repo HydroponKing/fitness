ddcb1e859f613e1c09a5be515306d9e4a0f30f68
HydroponKing <momomolchenel@gmail.com>
2024-10-04 01:22:04 +0500

    поменял название файла на CoursePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { AppRoutes } from './lib/appRoutes'
 import Main from './components/Main/Main'
-import Course from './components/Course/Course'
+import CoursePage from './components/CoursePage/CoursePage'
 
 function App() {
 	return (
 		<div className='container'>
 			<Routes>
 				<Route path={AppRoutes.MAIN} element={<Main />} />
-				<Route path={AppRoutes.COURSE} element={<Course />} />
+				<Route path={AppRoutes.COURSE} element={''} />
+				<Route path={AppRoutes.COURSEPAGE} element={<CoursePage />} />
 				<Route path={AppRoutes.WORKOUT_VIDEO} element={''} />
 				<Route path={AppRoutes.USER_PROFILE} element={''} />
 				<Route path={AppRoutes.LOGIN} element={''} />

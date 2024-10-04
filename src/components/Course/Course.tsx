24bb74265557a460923a82fde7d69f094fa21cd0
didusanatoliy <bereg.007@mail.ru>
2024-10-04 19:19:23 +0300

    мучаюсь со стилями

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -4,37 +4,40 @@ export type CourseOneType = {
 	course: courseType,
 }
 export default function Course({ course }: CourseOneType) {
-	const { nameRU,srcSmall} = course
+	const { nameRU, srcSmall } = course
 	return (
 		<>
-			<div className="relative shadow-lg max-w-[360px] mt-8 mb-2 flex flex-col self-start rounded-3xl bg-white sm:shrink-0 sm:grow sm:basis-0 box-border">
-				<div className="  mx-[0] mt-[0] max-w-[360px] max-h-[501px]">
-					<img className="rounded-3xl" src={srcSmall}/>
-					<svg className="w-[18px] h-[18px] absolute top-[20px] right-[40px]">
+			<div className="relative flex flex-col gap-[24px] w-[360px] 
+				mt-[40px] rounded-[30px] bg-white shadow-shadow_primary
+				mobile:flex-col mobile:items-center mobile:mt-[24px] mobile:w-[343px]">
+				<div className="overflow-hidden rounded-[30px] ">
+					<img className="rounded-3xl" src={srcSmall} />
+					<svg className="w-[18px] h-[18px] ">
 						<use xlinkHref="/src/assets/img/icon/sprite.svg#icon-plus" />
 					</svg>
 				</div>
-				<div className="p-[6] mb-[4] text-base grid md:gap-[3] ">
-					<h6 className="font-bold text-[32px] my-6">{nameRU}</h6>
+				<div className="h-[137px] text-base grid md:gap-[3] ml-12 mb-12;}">
+					<h6 className="text-[32px] font-medium leading-[35px]">{nameRU}</h6>
 					<div className="flex justify-start">
 						<div className=" flex justify-start rounded-3xl bg-[#F7F7F7] px-[3] py-[3] mr-[2]">
 							<svg className="w-[18px] h-[18px]">
 								<use xlinkHref="/src/assets/img/icon/sprite.svg#calendar" />
 							</svg>
-							<div className="pl-[2]" font-bold ></div>
+							<div className="pl-[2]" font-bold >25 дней</div>
 						</div>
 						<div className="flex justify-start rounded-3xl shadow-lg bg-[#F7F7F7] px-[3] py-[3] ">
 							<svg className="w-[18px] h-[18px]">
 								<use xlinkHref="/src/assets/img/icon/sprite.svg#time" />
 							</svg>
-							<div className="pl-[2]"></div>
+							<div className="pl-[2]">20-50 мин/день</div>
 						</div>
 					</div>
-					<div className="flex justify-start rounded-3xl bg-[#F7F7F7] px-[3] py-[3] mr-[24] ">
+					<div className="flex gap-[6px] items-center 
+							bg-gray_bg rounded-[50px]">
 						<svg className="w-[18px] h-[18px]">
 							<use xlinkHref="/src/assets/img/icon/sprite.svg#difficulty_signal" />
 						</svg>
-						<div className="pl-[2]"></div>
+						<div className="pl-[2]">Сложность</div>
 					</div>
 				</div>
 			</div>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,5 @@
 import Course from '../Course/Course'
 import Header from '../Header/Header'
-// import yoga from '/src/assets/img/yoga2.png'
-// import stretching from '/src/assets/img/stretching2.png'
-// import zumba from '/src/assets/img/zumba2.png'
-// import bodyflex from '/src/assets/img/bodyflex2.png'
-// import step from '/src/assets/img/step.png'
 import Button from '../Button/Button'
 import { useEffect, useState } from 'react'
 import { getCourses } from '../../api/courseApi'
@@ -22,57 +17,15 @@ export default function Main() {
 	console.log(courses);
 	const sortedCourses = [...courses].sort((a, b) => a.order - b.order)
 	
-	// const courses = [
-	// 	{
-	// 		id: 1,
-	// 		name: "Йога",
-	// 		src: yoga,
-	// 		duration: "25дней",
-	// 		timeaday: "20-25минут/день",
-	// 		level: "Сложность",
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		name: "Стретчинг",
-	// 		src: stretching,
-	// 		duration: "25дней",
-	// 		timeaday: "20-25минут/день",
-	// 		level: "Сложность",
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		name: "Зумба",
-	// 		src: zumba,
-	// 		duration: "25дней",
-	// 		timeaday: "20-25минут/день",
-	// 		level: "Сложность",
-	// 	},
-	// 	{
-	// 		id: 4,
-	// 		name: "Степ-аэробика",
-	// 		src: step,
-	// 		duration: "25дней",
-	// 		timeaday: "20-25минут/день",
-	// 		level: "Сложность",
-	// 	},
-	// 	{
-	// 		id: 5,
-	// 		name: "Бодифлекс",
-	// 		src: bodyflex,
-	// 		duration: "25дней",
-	// 		timeaday: "20-25минут/день",
-	// 		level: "Сложность",
-	// 	}
-	// ]
 	return (
 		<main>
 			<Header />
 			<div className="bg-gray-[100] grid place-content-center">
-				<div className=" max-w-[1440px]">
+				<div className=" max-w-[1440px] mt-5">
 					<div className="flex justify-between my-[50px] relative">
-						<div className="text-[60px] h-[120px] text-pretty inline-block align-middle"> Начните заниматься спортом и улучшите качество жизни</div>
-						<div className="mt-[6] p-[4] text-[32px] min-w-[288px] max-h-[120px] rounded-md bg-[#BCEC30]">Измени своё тело за полгода!</div>
-						<div className="absolute top-[134px] right-[150px]">
+						<div className="text-[60px] h-[120px] text-pretty inline-block align-middle font-bold"> Начните заниматься спортом и улучшите качество жизни</div>
+						<div className="mt-[6] p-[4] text-[32px] min-w-[288px] max-h-[102px] rounded-md bg-[#BCEC30]">Измени своё тело за полгода!</div>
+						<div className="absolute top-[90px] right-[150px]">
 							<img src="/src/assets/img/polygon.png" alt="polygon" />
 						</div>
 
@@ -83,13 +36,13 @@ export default function Main() {
 						))}
 					</div>
 					<div className="flex justify-center ">
-						<Button
-							width='w-[103px]'
-							background='bg-green_bg'
-							hover='bg-hover'
-							active='bg-active active:text-white'
-							title='Наверх ↑'
-						/>
+					<Button
+					width='w-[127px]'
+					background='bg-green_bg'
+					hover='hover:bg-hover'
+					active='active:bg-active active:text-white'					
+					title='Наверх ↑'
+				/>
 					</div>
 				</div>
 			</div>

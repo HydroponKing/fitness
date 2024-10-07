744b61d83de27087e3cba9bcad8ac414857e9ccf
HydroponKing <momomolchenel@gmail.com>
2024-10-07 16:50:59 +0500

    ручное добавление ветки

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { AppRoutes } from './lib/appRoutes'
 import Main from './components/Main/Main'
+import CoursePage from './components/CoursePage/CoursePage'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path={AppRoutes.MAIN} element={<Main />} />
 				<Route path={AppRoutes.COURSE} element={''} />
+				<Route path={AppRoutes.COURSEPAGE} element={<CoursePage />}/>
 				<Route path={AppRoutes.WORKOUT_VIDEO} element={''} />
 				<Route path={AppRoutes.USER_PROFILE} element={''} />
 				<Route path={AppRoutes.LOGIN} element={''} />

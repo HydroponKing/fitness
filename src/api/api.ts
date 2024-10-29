38c35125cb621e7be957e87e9f05ffb9b7713b78
didusanatoliy <bereg.007@mail.ru>
2024-10-29 20:57:10 +0300

    Доработки прогресса

--- src/api/api.ts
+++ src/api/api.ts
@@ -80,23 +80,6 @@ export const getUserWorkouts = async (userId: string, courseId: string) => {
 	}
 }
 
-// export const getVideo = async (workoutId: string) => {
-// 	// для подучения "воркаутов с видео" в Firebase
-// 	let result: WorkoutType | null = null
-
-// 	try {
-// 		const snapshot = await get(child(ref(db), `workouts/${workoutId}`))
-
-// 		if (snapshot.exists()) {
-// 			result = snapshot.val()
-// 		}
-// 	} catch (e) {
-// 		console.error(e)
-// 	}
-
-// 	return result?.video
-// }
-
 // Получение коллекции курсов пользователя по uid
 export const getUserCourses = async (
 	userId: string | undefined,
